147567b3152fa61c8c6df73e7d8af8ec0c16d489
Moritz Schmitz von Hülst <moritz@hauptstadtoffice.com>
2022-02-16 10:04:11 +0100

    init

---
+++ src/main/java/com/example/superchatbackendchallengemoritz/SuperchatBackendChallengeMoritzApplication.java
@@ -0,0 +1,13 @@
+package com.example.superchatbackendchallengemoritz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperchatBackendChallengeMoritzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperchatBackendChallengeMoritzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/superchatbackendchallengemoritz/SuperchatBackendChallengeMoritzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superchatbackendchallengemoritz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperchatBackendChallengeMoritzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
